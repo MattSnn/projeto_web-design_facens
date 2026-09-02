cfbb4e63554cca9f8ad04d002d1b08373494772a
AquaSquirel <aquasquirel@gmail.com>
2026-09-02 12:03:09 -0300

    feat: add NavBar component and integrate it into App

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+function NavBar()
+{
+    return(
+        <nav className="bg-green-400 w-full py-4 px-4  flex">
+            <a href="#inicio" className="font-black text-emerald-900 text-2xl" >Imobiliaria</a>
+            <div className="text-right flex justify-end w-full gap-5 text-shadow-emerald-950 text-lg font-bold">
+                <a href="#oportunidades">Oportunidades</a>
+                <a href="#contato">Contato</a>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
