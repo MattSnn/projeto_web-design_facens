0c0ec20934eaf24dc45e4f095461c0618c954880
PedroWagnerFlorentino <pedrowf1905@gmail.com>
2026-09-03 12:09:56 -0300

    fix: improve layout and styling of the NavBar component for better responsiveness in small screens

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,8 +2,8 @@ function NavBar()
 {
     return(
         <nav className="bg-green-400 w-full py-4 px-4 top-0 fixed flex">
-            <a href="#inicio" className="font-black text-emerald-900 text-2xl" >Imobiliaria</a>
-            <div className="text-right flex justify-end w-full gap-5 text-shadow-emerald-950 text-lg font-bold">
+            <a href="#inicio" className="font-black text-emerald-900 text-lg md:text-2xl" >Imobiliaria</a>
+            <div className="text-right flex justify-end w-full gap-2 md:gap-5 text-shadow-emerald-950 text-sm md:text-lg font-bold flex-wrap">
                 <a href="#oportunidades">Oportunidades</a>
                 <a href="#contato">Contato</a>
             </div>
