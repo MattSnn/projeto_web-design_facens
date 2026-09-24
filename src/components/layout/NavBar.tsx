402d6135c19f876fded580f0d8d0d83b59e8581a
AquaSquirel <aquasquirel@gmail.com>
2026-09-24 13:41:59 -0300

    fix: update NavBar component link text for branding consistency

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -2,7 +2,7 @@ function NavBar()
 {
     return(
         <nav className="bg-brand-dark w-full z-1 py-4 px-4 top-0 fixed flex items-center">
-            <a href="#inicio" className="font-black text-on-brand text-lg md:text-2xl" >Imobiliaria</a>
+            <a href="#inicio" className="font-black text-on-brand text-lg md:text-2xl" >ImobiFaces</a>
             <div className="text-right flex justify-end w-full gap-2 md:gap-5 text-shadow-brand-dark text-sm md:text-lg font-bold flex-wrap">
                 <a href="#oportunidades">Oportunidades</a>
                 <a href="#contato">Contato</a>
